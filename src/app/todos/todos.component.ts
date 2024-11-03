43ce5e9f2f1622f8c30478651fda00d11aa78784
janaka120 <janakastill@gmail.com>
2024-11-03 11:37:07 +0800

    add todos service class and render in todos component

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
+import { TodosService } from '../services/todos.service';
+import { Todo } from '../model/todo.type';
+import { NgFor, NgTemplateOutlet } from '@angular/common';
 
 @Component({
   selector: 'app-todos',
   standalone: true,
-  imports: [],
+  imports: [NgFor, NgTemplateOutlet],
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.scss'
 })
-export class TodosComponent {
+export class TodosComponent implements OnInit {
+  todoService = inject(TodosService);
+  todoItems = signal<Array<Todo>>([]);
 
+  ngOnInit(): void {
+    console.log(this.todoService.todoItems);
+    this.todoItems.set(this.todoService.todoItems)
+  }
 }
